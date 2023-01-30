b25cea8192c29f4b7ffc63328ec0789705b6a82c
guilhemkzk <guilhem.l.krawczyk@gmail.com>
2023-01-30 19:23:39 +0100

    Separating connexion.js from loadWorks.js + implementing gallery in modal + icons

---
+++ FrontEnd/js/connexion.js
@@ -0,0 +1,54 @@
+// --------------------- CONNEXION --------------------- //
+
+// Getting 'true' if the token exist, then the user is connected
+const isUserConnected = localStorage.getItem("token") !== null;
+
+// IF THE USER IS CONNECTED, DISPAY EDITION PARAMETERS
+if (isUserConnected) {
+  // Get all the HTML items with class "edition"
+  // and set their display to "flex" to make them visible
+  document
+    .querySelectorAll(".edition")
+    .forEach((a) => (a.style.display = "flex"));
+
+  // Display the logout option using "display : block"
+  let logoutOption = document.getElementById("logout");
+  logoutOption.style.display = "block";
+
+  // Hide the login option using "display : none"
+  let loginOption = document.getElementById("login");
+  loginOption.style.display = "none";
+
+  //Display the black edition bar using "display : flex"
+  let blackStripEdition = document.getElementById("bandeau-edition");
+  blackStripEdition.style.display = "flex";
+
+  //Hide the filtering options
+  let sortingDiv = document.getElementById("tri");
+  sortingDiv.style.display = "none";
+  // ELSE IF THE USER IS NOT CONNECTED
+} else {
+  // Get all the HTML items with class "edition"
+  //and set their display to "none"
+  document
+    .querySelectorAll(".edition")
+    .forEach((a) => (a.style.display = "none"));
+
+  // Hide the logout option using "display : none"
+  let logoutOption = document.getElementById("logout");
+  logoutOption.style.display = "none";
+  // Display the login option using "display : block"
+  let loginOption = document.getElementById("login");
+  loginOption.style.display = "block";
+  //Hide the black edition bar using "display : none"
+  let blackStripEdition = document.getElementById("bandeau-edition");
+  blackStripEdition.style.display = "none";
+}
+
+// FUNCTION LOGOUT
+async function logout() {
+  // Clear all local storage session
+  window.localStorage.clear();
+  // Redirect to the index.html page (reload)
+  location.href = "./index.html";
+}
--- FrontEnd/js/loadWorks.js
+++ FrontEnd/js/loadWorks.js
@@ -1,8 +1,25 @@
 // Get the container of the gallery of works
 let gallery = document.getElementById("gallery-container");
 
+// FUNCTION TO CREATE HTML FROM API WORKS IN ORDRE TO SEND IT TO THE GALLERY
+async function writeGallery(works) {
+  return (
+    '<div class="gallery">' +
+    works
+      .map(
+        (works) => `
+  <figure>
+    <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}" />
+    <figcaption>${works.title}</figcaption>
+    </figure>`
+      )
+      .join("") +
+    "</div>"
+  );
+}
+
 // FUNCTION TO GET ALL THE WORKS USING API CALL
-async function getWorks() {
+async function getWorks(location) {
   //intiate variable
   let allWorks;
   try {
@@ -11,30 +28,15 @@ async function getWorks() {
     // Wait for the answer (async) and send it to the variable
     allWorks = await res.json();
     // Write the works in the gallery with the dedicated fonction
-    const returnWorks = (allWorks) => {
-      // Function to create HTML from API response in order to send it to the gallery
-      return (
-        '<div class="gallery">' +
-        allWorks
-          .map(
-            (allWorks) => `
-      <figure>
-        <img crossorigin="anonymous" src="${allWorks.imageUrl}" alt="${allWorks.title}" />
-        <figcaption>${allWorks.title}</figcaption>
-        </figure>`
-          )
-          .join("") +
-        "</div>"
-      );
-    };
+    const returnWorks = await writeGallery(allWorks);
     // Send the content written in the HTML page (errasing what was written previously)
-    gallery.innerHTML = returnWorks(allWorks);
+    location.innerHTML = returnWorks;
   } catch (err) {
     console.log(err);
   }
 }
 
-getWorks();
+getWorks(gallery);
 
 // Get the id of the element containing the sorting buttons
 let filterDiv = document.getElementById("tri");
@@ -49,6 +51,7 @@ async function getCategories() {
     const res = await fetch("http://localhost:5678/api/categories");
     // Wait for the answer (async) and send it to the variable in json
     allCategories = await res.json();
+
     //Use the categories to create HTML buttons in order to send it to the page
     const returnCategories = (allCategories) => {
       return (
@@ -70,64 +73,91 @@ async function getCategories() {
 
 getCategories();
 
-// --------------------- CONNEXION --------------------- //
-
-// Getting 'true' if the token exist, then the user is connected
-const isUserConnected = localStorage.getItem("token") !== null;
-
-// IF THE USER IS CONNECTED, DISPAY EDITION PARAMETERS
-if (isUserConnected) {
-  // Get all the HTML items with class "edition"
-  // and set their display to "flex" to make them visible
-  document
-    .querySelectorAll(".edition")
-    .forEach((a) => (a.style.display = "flex"));
-
-  // Display the logout option using "display : block"
-  let logoutOption = document.getElementById("logout");
-  logoutOption.style.display = "block";
-  // Hide the login option using "display : none"
-  let loginOption = document.getElementById("login");
-  loginOption.style.display = "none";
-
-  //Display the black edition bar using "display : flex"
-  let blackStripEdition = document.getElementById("bandeau-edition");
-  blackStripEdition.style.display = "flex";
-
-  // ELSE IF THE USER IS NOT CONNECTED
-} else {
-  // Get all the HTML items with class "edition"
-  //and set their display to "none"
-  document
-    .querySelectorAll(".edition")
-    .forEach((a) => (a.style.display = "none"));
-
-  // Hide the logout option using "display : none"
-  let logoutOption = document.getElementById("logout");
-  logoutOption.style.display = "none";
-  // Display the login option using "display : block"
-  let loginOption = document.getElementById("login");
-  loginOption.style.display = "block";
-  //Hide the black edition bar using "display : none"
-  let blackStripEdition = document.getElementById("bandeau-edition");
-  blackStripEdition.style.display = "none";
-}
-
-// FUNCTION LOGOUT
-async function logout() {
-  // Clear all local storage session
-  window.localStorage.clear();
-  // Redirect to the index.html page (reload)
-  location.href = "./index.html";
-}
-
 // --------------------- MODAL --------------------- //
+
+// FUNCTION TO LOAD ALL THE WORKS IN THE MODAL
+
+// async function getEditWorks() {
+//   //intiate variable
+//   let allWorks;
+//   try {
+//     // Call API : GET all works
+//     const res = await fetch("http://localhost:5678/api/works");
+//     // Wait for the answer (async) and send it to the variable
+//     allWorks = await res.json();
+//     // Write the works in the gallery with the dedicated fonction
+//     const returnWorks = (allWorks) => {
+//       // Function to create HTML from API response in order to send it to the gallery
+//       return (
+//         '<div class="gallery">' +
+//         allWorks
+//           .map(
+//             (allWorks) => `
+//             <div>Test</div>
+//         <figure>
+//         <img crossorigin="anonymous" src="${allWorks.imageUrl}" alt="${allWorks.title}" />
+//         <figcaption>éditer</figcaption>
+//         </figure>`
+//           )
+//           .join("") +
+//         "</div>"
+//       );
+//     };
+//     // Send the content written in the HTML page (errasing what was written previously)
+//     gallery.innerHTML = returnWorks(allWorks);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 // OPEN THE MODAL //
 
 async function getModal() {
   var modal = document.getElementById("modale");
   modal.style.display = "flex";
+
+  // CREATING GALLERY IN THE MODAL
+
+  // Get the container of the gallery
+  let editGallery = document.getElementById("edit-gallery");
+
+  let allWorks;
+  try {
+    // Call API : GET all works
+    const res = await fetch("http://localhost:5678/api/works");
+    // Wait for the answer (async) and send it to the variable
+    allWorks = await res.json();
+    // Write the works in the gallery with the dedicated fonction
+    const returnWorks = (allWorks) => {
+      // Function to create HTML from API response in order to send it to the gallery
+      return (
+        '<div class="gallery">' +
+        allWorks
+          .map(
+            (allWorks) => `
+            <div class="modal-overlay-container">
+
+              <div class="trash-can-icon">
+                <i class="fa-regular fa-trash-can fa-sm"></i>
+              </div>
+        <figure>
+        <div class="size-icon" id="size-icon-${allWorks.id}">
+          <i class="fa-solid fa-arrows-up-down-left-right"></i>
+        </div>
+        <img id="modal-img-${allWorks.id}" crossorigin="anonymous" src="${allWorks.imageUrl}" alt="${allWorks.title}" />
+        <figcaption>éditer</figcaption>
+        </figure>
+        </div>`
+          )
+          .join("") +
+        "</div>"
+      );
+    };
+    // Send the content written in the HTML page (errasing what was written previously)
+    editGallery.innerHTML = returnWorks(allWorks);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // CLOSE THE MODAL //
@@ -145,11 +175,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-// // ------------ EVENT LISTENER FOR THE EDITING LINK FOR THE GALLERY ----------- //
-// window.addEventListener("load", function () {
-//   // Get the button that opens the modal
-//   let openModalLink = document.getElementById("edition-gallery");
-//   openModalLink.addEventListener("click", function () {
-//     modal.style.display = "block";
-//   });
-// });
